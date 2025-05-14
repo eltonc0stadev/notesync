33419d088df1173a394530b8c2e7784b199e72a3
starlords2 <caiopantoja9@gmail.com>
2025-05-14 15:53:17 -0300

    novas janelas e funções

--- app/src/main/java/com/example/treino1/Janelaprincipal.kt
+++ app/src/main/java/com/example/treino1/Janelaprincipal.kt
@@ -1,13 +1,58 @@
 package com.example.treino1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.jvm.java
 
 class Janelaprincipal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.telaprincipal)
+
+        val layoutincluido = findViewById<View>(R.id.include_grupo_nota)
+
+        val testedenota = layoutincluido.findViewById<ImageView>(R.id.bloconota)
+
+        testedenota.setOnClickListener {
+            val intent = Intent(this, Telanota::class.java)
+            startActivity(intent)
+        }
+//codigo faz com q mude as imagens
+        val favoritoemp = layoutincluido.findViewById<ImageView>(R.id.favoritoempty)
+        val favoritofull = layoutincluido.findViewById<ImageView>(R.id.favselecionado)
+
+        val lockopen = layoutincluido.findViewById<ImageView>(R.id.lockopen)
+        val lockclose = layoutincluido.findViewById<ImageView>(R.id.lockclose)
+
+        favoritoemp.setOnClickListener {
+            favoritoemp.visibility = View.INVISIBLE
+            favoritofull.visibility = View.VISIBLE
+        }
+        favoritofull.setOnClickListener {
+            favoritofull.visibility = View.INVISIBLE
+            favoritoemp.visibility = View.VISIBLE
+        }
+        lockopen.setOnClickListener {
+            lockopen.visibility = View.INVISIBLE
+            lockclose.visibility = View.VISIBLE
+        }
+        lockclose.setOnClickListener {
+            lockclose.visibility = View.INVISIBLE
+            lockopen.visibility = View.VISIBLE
+        }
+
+        val perfil = findViewById<ImageView>(R.id.iconeperfil)
+
+        perfil.setOnClickListener {
+            val intentperf = Intent(this, Telaperfil::class.java)
+            startActivity(intentperf)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/treino1/Telanota.kt
+++ app/src/main/java/com/example/treino1/Telanota.kt
@@ -1,13 +1,26 @@
 package com.example.treino1
 
+
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+
 
 class Telanota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.telanota)
+
+        val voltar = findViewById<ImageButton>(R.id.voltarnota)
+
+        voltar.setOnClickListener {
+            val intent = Intent(this, Janelaprincipal::class.java)
+            startActivity(intent)
+        }
+
     }
 }
